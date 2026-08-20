0c4a7b75c4439ec60c9c53bcb9ef5ab861e528a5
Elsharabasy73 <abdo.make73@gmail.com>
2026-08-20 11:58:02 +0300

    feat: implement category management with CRUD operations and validation

--- src/modules/employees/employee.routes.js
+++ src/modules/employees/employee.routes.js
@@ -1,5 +1,47 @@
 const express = require("express");
 
 const router = express.Router();
+const {
+  createEmployee,
+  getAllEmployee,
+  getEmployee,
+  updateEmployee,
+  deleteEmployee,
+} = require("./employee.controller");
+
+const { protect, allowTo } = require("../../middlewares/auth.middleware");
+
+const {
+  createEmployeeValidator,
+  getEmployeeValidator,
+  updateEmployeeValidator,
+  deleteEmployeeValidator,
+} = require("./employee.validation");
+
+router
+  .route("/")
+  .get(getAllEmployee)
+  .post(
+    protect,
+    allowTo(["admin"]),
+    createEmployeeValidator,
+    createEmployee,
+  );
+
+router
+  .route("/:id")
+  .get(getEmployeeValidator, getEmployee)
+  .put(
+    protect,
+    allowTo(["admin"]),
+    updateEmployeeValidator,
+    updateEmployee,
+  )
+  .delete(
+    protect,
+    allowTo(["admin"]),
+    deleteEmployeeValidator,
+    deleteEmployee,
+  );
 
 module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,8 @@ const router = express.Router();
 
 const authRoutes = require("../modules/auth/auth.routes");
 const galleryRoutes = require("../modules/galleries/gallery.routes");
-const categoryRoutes = require("../modules/category/category.routes");
+const categoryRoutes = require("../modules/categories/category.routes");
+const empoloyRoutes = require("../modules/employees/employee.routes");
 
 router.get("/health", (req, res) => {
   res.status(200).json({
@@ -19,5 +20,6 @@ router.get("/health", (req, res) => {
 router.use("/auth", authRoutes);
 router.use("/galleries", galleryRoutes);
 router.use("/categories", categoryRoutes);
+router.use("/employees",empoloyRoutes)
 
 module.exports = router;
